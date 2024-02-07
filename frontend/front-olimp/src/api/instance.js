26ebe29a7196b7c4ebc1b962265a95eb7cdb9e23
keyready <vip.korchak@list.ru>
2024-02-08 00:54:07 +0300

    поставил время олимпиады

--- frontend/front-olimp/src/api/instance.js
+++ frontend/front-olimp/src/api/instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    // baseURL: 'https://olimpiadvka.ru' // вставьте свой url
-    baseURL: 'http://localhost:3000/' // вставьте свой url
+    baseURL: 'https://olimpiadvka.ru' // вставьте свой url
+    // baseURL: 'http://localhost:3000/' // вставьте свой url
 })
 export default instance
